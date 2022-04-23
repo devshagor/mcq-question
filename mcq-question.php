286fe352b9f89a3e9f5aaf14f4deb83fe6b12347
devshagor <shagors333@gmail.com>
2022-04-23 15:53:49 +0600

    Create mcq-question.php

---
+++ mcq-question.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name:       MCQ Question
+ * Description:        Multiple-choice question(MCQ) system using the latest version of WordPress
+ * Requires at least: 5.9
+ * Requires PHP:      7.0
+ * Version:           1.0.0
+ * Author:            devshagor
+ * License:           GPL-2.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       mcq-question
+ *
+ * @package           create-block
+ */
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
